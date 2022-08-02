1d59a0f71466e6b66cc4d9f9d8315b7628719b0d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-08-02 15:47:30 +0200

    landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './sass/style.scss';
+import { FC } from "react";
+import question from './assets/question.png';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+          <section className="top">
+              <h1>
+                  Êtes vous un
+                  <span>super héros</span>
+                  <span>de la communication</span>
+                  de rentrée ?
+              </h1>
+          </section>
+          <section className="bottom">
+              <button className="btn no-btn">Pas du tout <span className="pseudo-bg">Pas du tout </span></button>
+              <div className="questionMen" style={{ background: `url(${question}) no-repeat center 0 ` }} ></div>
+              <button className="btn yes-btn">Carrément<span className="pseudo-bg">Carrément</span></button>
+          </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
